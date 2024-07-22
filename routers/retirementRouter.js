8e35b9f7d0ca40cb14903217ee234695dd74c76d
Shyamak Seth <sethshyamak@gmail.com>
2024-07-22 21:11:14 +0530

    pension ki bakchodi

--- routers/retirementRouter.js
+++ routers/retirementRouter.js
@@ -2,7 +2,20 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
     if (!req.user) return res.redirect('/login')
-    res.render('retirement', {user: req.user, applyForRet: true, pension: 2000})
+    var myPension = 0
+    if (req.user.age < 60) {
+        return res.render('retirement', {user: req.user, applyForRet: false, pension: 0})
+    }
+    if (req.user.loyaltyLevel == 'Bronze') {
+        myPension = 0
+    } else if (req.user.loyaltyLevel == 'Silver') {
+        myPension = 3000
+    } else if (req.user.loyaltyLevel == 'Gold') {
+        myPension = 4000
+    } else if (req.user.loyaltyLevel == 'Platinum') {
+        myPension = 5000
+    }
+    res.render('retirement', {user: req.user, applyForRet: true, pension: myPension})
 })
 
 module.exports = router
